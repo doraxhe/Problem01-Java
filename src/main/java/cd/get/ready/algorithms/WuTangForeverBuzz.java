c3988e47c6c705cb33c5911b0c16467b8216b7dd
doraxhe <doraapplication@gmail.com>
2020-10-06 09:47:51 -0400

    Finished.

--- src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/cd/get/ready/algorithms/WuTangForeverBuzz.java
@@ -2,7 +2,25 @@ package cd.get.ready.algorithms;
 
 public class WuTangForeverBuzz {
 
-    public String wuTangClan(){
-        return null;
+    public String wuTangClan(int value){
+        String print;
+        StringBuilder string = new StringBuilder();
+
+        for (int i = 0; i < value; i++) {
+            if (((i + 1) % 3) == 0 && ((i + 1) % 5) != 0) {
+                print = "Wu\n";
+            }
+            else if (((i + 1) % 5) == 0 && ((i + 1) % 3) != 0) {
+                print = "Tang\n";
+            }
+            else if ((i + 1) % 15 == 0) {
+                print = "WuTangForever\n";
+            }
+            else {
+                print = (i + 1) + "\n";
+            }
+            string.append(print);
+        }
+        return string.toString();
     }
 }
